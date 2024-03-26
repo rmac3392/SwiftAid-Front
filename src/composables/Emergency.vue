4426e34733ee825065d81613fecc465a80b03f81
Mtonilon22 <mtonilon17@gmail.com>
2024-03-26 14:28:19 +0800

    revised design and added modal in operator's page

--- src/composables/Emergency.vue
+++ src/composables/Emergency.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex justify-center items-center h-7 w-[150px] text-sm p-1 bg-secondary text-primary rounded-full font-semibold border-black border-2 transition duration-300"
+    class="flex justify-center items-center h-10 text-center w-[150px] text-sm py-5 bg-secondary text-primary rounded-lg mx-2 my-2 font-semibold border-black border-2 transition duration-300"
   >
     {{ department }}
   </div>
